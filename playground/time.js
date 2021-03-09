2f6c21d8b4d053132de5b5606732786b4721055f
dmoriley <david.m.oriley@hotmail.com>
2021-03-09 10:34:49 -0500

    Updated packages

--- playground/time.js
+++
@@ -1,9 +0,0 @@
-const moment = require('moment');
-
-var createdAt = 1234
-var date = moment('2:35 am', 'h:mm a');
-date.add(1.5,'minutes');
-console.log(date.valueOf());
-console.log(date.format('MMMM Do, YYYY'));
-console.log(date.format('h:mm a'))
-
