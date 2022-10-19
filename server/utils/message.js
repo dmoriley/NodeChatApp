3af31878a5f90bd484c7dbbfe6aafee34b96b850
dmoriley <david.m.oriley@hotmail.com>
2022-10-19 16:09:52 -0400

    Removed moment from server side

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,16 +1,5 @@
-const moment = require('moment');
-
-var generateMessage = (from, text,colour) => {
-    return{from,text,createdAt:moment().valueOf(),colour};
+var generateMessage = (from, text, colour) => {
+  return { from, text, createdAt: Date.now(), colour };
 };
 
-var generateLocationMessage = (from, lat, long,colour) => {
-    return{
-        from,
-        url: `https://www.google.com/maps?q=${lat},${long}`,
-        createdAt: moment().valueOf(),
-        colour
-    }
-};
-
-module.exports = {generateMessage, generateLocationMessage};
+module.exports = { generateMessage };
